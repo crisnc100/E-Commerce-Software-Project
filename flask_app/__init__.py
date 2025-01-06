16745aa00df75ec18a6346a9ed605327fafb353a
crisnc100 <crisnc100@gmail.com>
2025-01-06 16:07:15 -0500

    chaning route for frontend live deployment

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Initialize Flask app
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins="*")  # or specify your origin
+CORS(app, supports_credentials=True, origins="https://ortega-shop.vercel.app")  # or specify your origin
 
 
 app.secret_key = os.getenv('SECRET_KEY')
