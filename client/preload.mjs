d32f31a6634e90dd47bf0eee6c5c72984a6edef2
crisnc100 <crisnc100@gmail.com>
2025-01-06 20:36:10 -0500

    Frontend changes

--- client/preload.mjs
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-// Expose APIs to the renderer process
-contextBridge.exposeInMainWorld('api', {
-    send: (channel, data) => ipcRenderer.send(channel, data),
-    receive: (channel, callback) =>
-        ipcRenderer.on(channel, (event, ...args) => callback(...args)),
-});
