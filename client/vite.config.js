6cabc0c4c1914add87e05ff3dc0aa781ab71a77f
crisnc100 <crisnc100@gmail.com>
2025-01-22 14:20:48 -0500

    backend live deployment again.

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mariaortegas-project.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
